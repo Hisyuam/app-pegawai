503d44429915e35ad1e772c5a47ac4f941ed1eea
Hisyuam <hisyamuddin2311@gmail.com>
2025-09-30 18:36:54 +0700

    CRUD Tabel Employee

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+
+
+Route::resource('employees', EmployeeController::class);
 
 Route::get('/', function () {
     return view('welcome');
